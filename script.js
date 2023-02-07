288614fb78093afcd408cd93ed772c144117eb7d
Nika <krasnikova16071607@gmail.com>
2023-02-07 13:25:25 +0300

    end

---
+++ script.js
@@ -0,0 +1,12 @@
+// Задание 1
+
+let tempCel = 15;
+let tempFa = tempCel * (9 / 5) + 32;
+console.log(`Температура в Цельсиях: ${tempCel}
+Температура в Фаренгейтах: ${tempFa}`);
+
+// Задание 2
+
+const myName = 'Анастасия';
+let admin = myName;
+console.log(`admin: ${admin}`);
