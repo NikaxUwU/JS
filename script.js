98b270086a5eb711cfe81721a7d11815aa719466
Nika <krasnikova16071607@gmail.com>
2023-02-07 17:25:18 +0300

    HW 2

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+//HW1
+
+// Задание 1
+
+// let tempCel = 15;
+// let tempFa = tempCel * (9 / 5) + 32;
+// console.log(`Температура в Цельсиях: ${tempCel}
+// Температура в Фаренгейтах: ${tempFa}`);
+
+// Задание 2
+
+// const myName = 'Анастасия';
+// let admin = myName;
+// console.log(`admin: ${admin}`);
+
+
+//HW2
+
 // Задание 1
 
 let num1 = prompt("Введите первое  число");
